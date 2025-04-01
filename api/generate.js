6dc3e2f42ba6f2541abdfbefb657058ba13e2da3
cuongtran1412 <quocuong2709@gmail.com>
2025-04-01 19:20:37 +0700

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
 
   let apparelDescription = apparel;
   if (apparel === "pajama") {
-    apparelDescription = "a full-body dog onesie";
+    apparelDescription = "a full-body dog bodysuit";
   } else if (apparel === "t shirt") {
     apparelDescription = "dog shirt sleeveless ";
   }
